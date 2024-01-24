64409964808f50e8f55c3bd0bebd2af2fb68ee6b
Jakub Grotha <jakub.grotha@gmail.com>
2024-01-24 21:22:46 +0100

    Replace RestTemplate with RestClient

--- src/main/java/com/example/libraryapp/service/GoogleBooksApiService.java
+++ src/main/java/com/example/libraryapp/service/GoogleBooksApiService.java
@@ -6,31 +6,26 @@ import com.example.libraryapp.model.google_books.GoogleBooksDetails;
 import com.example.libraryapp.model.google_books.Volume;
 import com.example.libraryapp.model.google_books.VolumeInfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @RequiredArgsConstructor
 @Service
 public class GoogleBooksApiService {
 
     private final GoogleBooksApiConfiguration configuration;
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     public BookDetails findBookDetailsInGoogleBooksApiUsingIsbn(String isbn) {
         if (!configuration.isEnabled()) {
             return new BookDetails();
         }
-        ResponseEntity<GoogleBooksDetails> response = restTemplate
-                .exchange("https://www.googleapis.com/books/v1/volumes?q=isbn:%s&keyes&key=%s"
-                                .formatted(isbn, configuration.getKey()),
-                        HttpMethod.GET,
-                        HttpEntity.EMPTY,
-                        new ParameterizedTypeReference<GoogleBooksDetails>() {
-                        });
+        String apiKey = configuration.getKey();
+        ResponseEntity<GoogleBooksDetails> response = restClient.get()
+                .uri(STR."https://www.googleapis.com/books/v1/volumes?q=isbn:\{isbn}&keyes&key=\{apiKey}")
+                .retrieve()
+                .toEntity(GoogleBooksDetails.class);
 
         GoogleBooksDetails googleBooksDetails = response.getBody();
         if (googleBooksDetails == null) {
